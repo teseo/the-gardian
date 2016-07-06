30dda690471a319a88385cb6e85c8a7edd70ed05
teseo <sol@javiermellado.com>
2016-07-06 17:57:58 +0100

    removing api key from query url

--- utils/core.js
+++ utils/core.js
@@ -4,8 +4,9 @@ export default function get(url){
 }
 
 export default function searchFor(query, offset){
+  const apiKey = 'api-key';
   const requestUrl = (
-    `http://content.guardianapis.com/search?show-elements=all&show-blocks=body&show-fields=lastModified,byline,headline,trailText,thumbnail,body&page-size=20&page=${ offset }&q=${ query }&type=article&api-key=api-key&order-by=newest`
+    `http://content.guardianapis.com/search?show-elements=all&show-blocks=body&show-fields=lastModified,byline,headline,trailText,thumbnail,body&page-size=20&page=${ offset }&q=${ query }&type=article&api-key=${ apiKey }&order-by=newest`
   );
 
   return get(requestUrl)
